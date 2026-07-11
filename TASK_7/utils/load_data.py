eab3c824d0955f526faaf41b0765f359d3e10a2b
Vignesh Thondikulam <vignesh.tho2006@gmail.com>
2026-07-11 16:00:08 -0400

    csv error fixing

--- TASK_7/utils/load_data.py
+++ TASK_7/utils/load_data.py
@@ -1,7 +1,9 @@
 import pandas as pd 
 import streamlit as st
-
+import os
 
 @st.cache_data
-def load_results(path="..\TASK_7\data\experiment_results.csv"):
-    return pd.read_csv(path)
+def load_results():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_path = os.path.join(current_dir, "data", "experiment_results.csv")
+    return pd.read_csv(csv_path)
